1f3d5b5af1647658ca84cd470222bf8e7680743b
Aleksandar Gakovic <algakovic@gmail.com>
2020-09-06 03:15:43 +0100

    change all instances of response == to response = and fix minor typo in code

--- advanced.py
+++ advanced.py
@@ -13,8 +13,7 @@ class geometry_solver(object):
     
     @staticmethod
     def volume_of_box(width, length, depth):
-        area = width*length
-        vol = area + depth
+        vol = width*length*depth
         return (vol)
 
 
@@ -46,18 +45,18 @@ def main():
             ans = geometry_solver.area_of_circle(r)
             print(f"Area of circle with radius {r} is {ans}")
         elif choice == "2":
-            response == input("Width? ")
+            response = input("Width? ")
             w = float(response)
             response = input("Length? ")
             l = float(response)
             ans = geometry_solver.area_of_rect(w, l)
             print(f"Area of Recatngle with width {w} and length {l} is {ans}")
         elif choice == "3":
-            response == input("Width? ")
+            response = input("Width? ")
             w = float(response)
-            response == input("Length? ")
+            response = input("Length? ")
             l = float(response)
-            response == input("Depth? ")
+            response = input("Depth? ")
             d = float(response)
             ans = geometry_solver.volume_of_box(w, l, d)
             print(f"Volume of box with Width {w}, Length {l} and Depth {d} is {ans}")
