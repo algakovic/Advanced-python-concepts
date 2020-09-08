ea9641906d84903b0660ab7f234c7b563ceb147e
Aleksandar Gakovic <algakovic@gmail.com>
2020-09-08 03:40:57 +0100

    use newly renamed testfile.txt to serve as the path.split to obtain root_dir

--- file_system_shell_methods.py
+++ file_system_shell_methods.py
@@ -5,8 +5,9 @@ import os
 from os import path
 import shutil
 from shutil import make_archive
+
 def main():
-    # make duplicate of an existing file
+    '''make duplicate of an existing file'''
     if path.exists("textfile.txt"):
         # get path to file in current directory
         src = path.realpath("textfile.txt")
@@ -15,17 +16,18 @@ def main():
         dst = src + ".bak"
 
         # copy the file:
-        # shutil.copy(src, dst)
+        '''shutil.copy(src, dst)'''
         # use shutil.copystat() for permissions, modification times and other info
 
         # Rename the original file
-        # os.rename("textfile.txt", "testfile.txt")
+        '''os.rename("textfile.txt", "testfile.txt")'''
 
-        '''Put things into a Zip Archive''' 
+    '''Put things into a Zip Archive'''
+    if path.exists("testfile.txt"):
+        src = path.realpath("testfile.txt")
         # use path.split to get directory path
-
         root_dir, tail = path.split(src)
-        shutil.make_archive("my_archive", "zip", root_dir)
+        shutil.make_archive("archive", "zip", root_dir)
 
 if __name__ == "__main__":
     main()
